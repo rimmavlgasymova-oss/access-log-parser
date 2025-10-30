85204afc0586e7b85eb2ae8e39e2cc368db209d7
rimmavl gasymova <git.user.rimma.vl.gasymova@gmail.com>
2025-10-30 19:49:44 +0300

    работа с числами

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-   System.out.println("Введите текст и нажмите <Enter>: " );
-String text = new Scanner(System.in).nextLine();
-System.out.println("Длина текста: " + text.length());
+        System.out.println("Введи первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Введи второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+
+        System.out.println("Сумма: " + sum);
+        System.out.println("Вычитание: " + difference);
+        System.out.println("Умножение: " + product);
+        System.out.println("Деление: " + quotient);
     }
-
-
 }
