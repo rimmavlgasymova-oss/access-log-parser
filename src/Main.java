726218dde98c374e54d5908f9e0e5624dc672ec1
rimmavl gasymova <git.user.rimma.vl.gasymova@gmail.com>
2025-11-18 19:10:29 +0300

    циклы курсовая

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-   System.out.println("Случайное число от 0 до 1: " + Math.random());
+        /*
+        System.out.println("Случайное число от 0 до 1: " + Math.random());*/
+        Scanner scanner = new Scanner(System.in);
+        int fileCount = 0;
 
+        while (true) {
+            System.out.print("Укажи путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                if (!fileExists) {
+                    System.out.println("Файл не существует");
+                } else {
+                    System.out.println("'это не файл");
+                }
+                continue;
+            }
+
+            fileCount++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + fileCount);
+        }
     }
-
-
 }
